df27f14631b816ba8c8e9f9c338737f4198c4dfe
Jeff Clegg <jeffclegg@att.net>
2020-11-12 23:25:06 -0500

    tweaked some code in generateMarkdown.js

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -8,7 +8,7 @@ function generateMarkdown(userData,userInfo) {
   This project has been deployed to my Personal GitHub Page. To get this project up and running, you can follow the deployment links that I have included in the Link Section below.;
   
    
-    # Table of Contents
+  # Table of Contents
   * [Description](#Project Overview)
   * [Links](#Links)
   * [Demo](#Demo)
@@ -25,41 +25,26 @@ function generateMarkdown(userData,userInfo) {
   
   #${userData.description}
 
-  
-
-  //Links Section
 
   ## Links
   
   *[Github Repository](https://github.com/${userData.userName}/${userData.repo})
   
 
-  // Demo Video
-
   ## Demo
 
   [Demo Video](https://github.com/${userData.userName}/${userData.repo}/blob/main/${userData.videoSrc}
 
-
-  // Installation section
    
   
   ## Installation
   
   Download (and unpack) or clone the repo, then using a CLI run the ${userData.usage} command.
   
-
-  // Usage section
-   
   
   ## Usage 
     
   Run the application with the CLI command ${userData.installation} and follow the prompts.
-  
-  
-  
-  // Contributing section
-
   
   
   ## Contributing
@@ -103,9 +88,9 @@ if (userData.tests === "") { markTest +=
     
   Please contact me by on of the following links below:
 
- |--|
+
   GitHub: [@${userInfo.login}](${userInfo.html_url})
-  |--|
+
   Email: ${userData.emailAddress}
   
 `
